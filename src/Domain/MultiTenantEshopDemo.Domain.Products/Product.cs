c0e4548bbf8dd5eb67354094d076e81059626d59
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-06-02 03:07:26 +0330

    feat: Implement product model

---
+++ src/Domain/MultiTenantEshopDemo.Domain.Products/Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MultiTenantEshopDemo.Domain.Products;
+
+public class Product
+{
+    public Guid Id { get; protected set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public required string Image { get; set; }
+}
